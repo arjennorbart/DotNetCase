0678f65a1ce27268bc9ab942f2117ac65153d506
unknown <arjennorbart@gmail.com>
2022-02-21 15:15:04 +0100

    Minor fix

--- src/Models/WeatherForecast.cs
+++ src/Models/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace ApiDotNetCase
     {
         [Key] public int Id { get; set; }
         public DateTime? Date { get; set; }
-        public int? TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         public string? Summary { get; set; }
         
@@ -14,7 +14,7 @@ namespace ApiDotNetCase
         {
         }
 
-        public WeatherForecast(int id, DateTime? date, int? temperatureC, string? summary)
+        public WeatherForecast(int id, DateTime? date, int temperatureC, string? summary)
         {
             Id = id;
             Date = date;
--- src/Presentation/Controllers/Dto/WeatherForecastDto.cs
+++ src/Presentation/Controllers/Dto/WeatherForecastDto.cs
@@ -3,7 +3,7 @@
     public class WeatherForecastDto
     {
         public DateTime? Date { get; set; }
-        public int? TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
         public string? Summary { get; set; }
     }
 }
